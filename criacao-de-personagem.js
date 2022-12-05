5ec564ec421850900642ab82d0bffbe048efd94b
Leonardo Arthur <lraguiar.la@gmail.com>
2022-12-05 15:44:24 -0300

    A volta dos que se perderam no tempo

--- criacao-de-personagem.js
+++ criacao-de-personagem.js
@@ -1,1 +1,5 @@
-console.log("Rodando o aplicativo de criação de personagem")
+console.log('Rodando o aplicativo de criação de personagem')
+
+console.log(
+  'Na praia de boa viagem, e foi pra ela que eu escrevi meu primeiro blues'
+)
